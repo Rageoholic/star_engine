81cf5d2103f5295cfa251ab6fed4dd7d3e23babe
Hersh Krishna <hershkrishna@Protonmail.ch>
2022-01-24 14:53:38 -0600

    Decide to make a switch to winit and erupt.

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,16 @@
-use bevy::prelude::*;
+use winit::{event_loop::{EventLoop, ControlFlow}, window::WindowBuilder, event::{Event, StartCause, WindowEvent}};
 
-#[derive(Component, Debug)]
-struct Person;
-#[derive(Component, Debug)]
-struct Name(String);
-
-struct GreetTimer(Timer);
-
-impl std::ops::Deref for GreetTimer {
-    type Target = Timer;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-fn hello_person(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("Hello {}", name.0);
-        }
-    }
-}
-
-fn add_people(mut commands: Commands) {
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Ada".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Kylie".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Lexie".to_string()));
-}
 
 fn main() {
-    App::new()
-        .insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(add_people)
-        .add_system(hello_person)
-        .run();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().with_title("starts").with_resizable(false).build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow|{
+        match event {
+            Event::WindowEvent{event: WindowEvent::CloseRequested, window_id} if window_id == window.id()=>*control_flow = ControlFlow::Exit,
+            Event::NewEvents(StartCause::Init)=>*control_flow=ControlFlow::Poll,
+            _ => {},
+
+        }
+    })
 }
